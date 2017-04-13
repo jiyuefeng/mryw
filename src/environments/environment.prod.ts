7b3b303897cbb040a292ec9649369eb3db1bb740
moc <758548361@qq.com>
2017-04-13 10:45:52 +0800

    修改若干

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   key: 'e3047bcd-2681-4a2a-a321-c75e64d89381',
   apiVersion: '1',
-  serverUrl: 'http://localhost:8080/api/'
+  serverUrl: 'http://106.2.112.147:81/api/'
 };
